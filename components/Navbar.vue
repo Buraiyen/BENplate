71663073b2d2057837ebb882c2f75c747ecac6ef
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-09-30 00:07:26 -0500

    :sparkles: init: navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav>
+    <ul
+      class="list-none flex flex-row items-center justify-center space-x-8 p-0"
+    >
+      <li v-for="link in links" :key="link">
+        <NuxtLink :to="link.path">
+          <div :class="`${link.icon} text-3xl`" />
+        </NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          name: 'Home',
+          path: '/',
+          icon: 'i-solar-home-2-broken',
+        },
+        {
+          name: 'About',
+          path: '/about',
+          icon: 'i-solar-sunset-broken'
+        },
+      ]
+    }
+  }
+}
+</script>
